e7ff9dfea6965c071b5b58ecb91fd55f522aa794
alvindioprakosa <alvindioprakosa@gmail.com>
2025-04-24 14:36:48 +0700

    Update Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from "next";
 import React from "react";
 import Image from "next/image";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
 
 /**
  * Home Page of the Application
@@ -13,7 +12,6 @@ const Home: NextPage = () => {
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, alias. Amet ea neque autem mollitia facere inventore rerum deserunt, laborum enim esse vel beatae vitae, iusto eaque sit a sunt.";
 
   const renderParagraphs = () => {
-    // Render the paragraph 20 times
     return Array.from({ length: 20 }, (_, index) => (
       <p key={index}>{paragraphText}</p>
     ));
@@ -21,19 +19,17 @@ const Home: NextPage = () => {
 
   return (
     <Layout pageTitle="Home Page">
-      <div className={styles["home-content"]}>
-        {/* Main image */}
-        <Image src="/heloo.jpg" width={200} height={200} alt="Helo" />
-        
-        {/* Title */}
-        <p className={styles["title-homepage"]}>Heloo</p>
+      {/* Main image */}
+      <Image src="/heloo.jpg" width={200} height={200} alt="Helo" />
 
-        {/* Rendered paragraphs */}
-        {renderParagraphs()}
+      {/* Title */}
+      <h1>Heloo</h1>
 
-        {/* Another image */}
-        <Image src="/b.jpg" width={200} height={200} alt="Blockchain" />
-      </div>
+      {/* Rendered paragraphs */}
+      {renderParagraphs()}
+
+      {/* Another image */}
+      <Image src="/b.jpg" width={200} height={200} alt="Blockchain" />
     </Layout>
   );
 };
