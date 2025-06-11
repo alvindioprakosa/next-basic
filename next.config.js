8097a85ea8c24dc747ae54b8283571d18d10f63d
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-11 10:33:00 +0700

    Update Next Config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+    formats: ['image/avif', 'image/webp'],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig;
