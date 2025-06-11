fb5bde5b51246eaaafa3836fdc2c17d2b5ae6949
alvindioprakosa <alvindioprakosa@gmail.com>
2025-06-11 10:33:51 +0700

    Add Jest Config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+  ],
+  testMatch: [
+    '<rootDir>/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig); 
